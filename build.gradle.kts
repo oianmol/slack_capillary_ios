180a7ec32f265542291b807706cbe33725b1e820
anmol verma <anmol.verma4@gmail.com>
2022-11-16 19:20:06 +0530

    update: code refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,41 @@
 val sdks = listOf("iphoneos", "iphonesimulator")
 
 sdks.forEach { sdk ->
-    tasks.create<Exec>("build${sdk.capitalize()}") {
-        group = "build"
-        description = "Builds $sdk"
+  tasks.create<Exec>("build${sdk.capitalize()}") {
+    group = "build"
+    description = "Builds $sdk"
 
-        val libraryName = "capillaryios"
-        commandLine(
-            "xcodebuild",
-            "-workspace", "$projectDir/$libraryName.xcworkspace",
-            "-scheme", libraryName,
-            "-derivedDataPath", "$projectDir/build",
-            "-destination", "generic/platform=iOS${ if (sdk == "iphonesimulator") " Simulator" else "" }",
-            "-sdk", sdk,
-            "-configuration", "Release", "SKIP_INSTALL=NO"
-        )
+    val libraryName = "capillaryios"
+    commandLine(
+      "xcodebuild",
+      "-workspace", "$projectDir/$libraryName.xcworkspace",
+      "-scheme", libraryName,
+      "-derivedDataPath", "$projectDir/build",
+      "-destination", "generic/platform=iOS${if (sdk == "iphonesimulator") " Simulator" else ""}",
+      "-sdk", sdk,
+      "-configuration", "Release", "SKIP_INSTALL=NO"
+    )
 
-        workingDir(projectDir)
+    workingDir(projectDir)
 
-        inputs.files(
-            fileTree("$projectDir/$libraryName.xcodeproj") { exclude("**/xcuserdata") },
-            fileTree("$projectDir/$libraryName")
-        )
-    }
+    inputs.files(
+      fileTree("$projectDir/$libraryName.xcworkspace") { exclude("**/xcuserdata") },
+      fileTree("$projectDir/$libraryName.xcodeproj") { exclude("**/xcuserdata") },
+      fileTree("$projectDir/$libraryName/Pods"),
+      fileTree("$projectDir/$libraryName")
+    )
+  }
 }
 
 tasks.create<Delete>("clean") {
-    group = "build"
-    delete("$projectDir/build")
+  group = "build"
+  delete("$projectDir/build")
 }
 
 tasks.register("buildCapillaryLibrary") {
-    group = "build"
-    description = "Builds all iOS dependencies"
+  group = "build"
+  description = "Builds all iOS dependencies"
 
-    val dependencies = sdks.map { name -> "build${name.capitalize()}" }
-    dependsOn(dependencies)
+  val dependencies = sdks.map { name -> "build${name.capitalize()}" }
+  dependsOn(dependencies)
 }
