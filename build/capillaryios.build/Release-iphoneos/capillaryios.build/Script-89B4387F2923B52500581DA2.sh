5ce8654d1b88a0966df58b35fe7a067b3ecef75a
anmol verma <anmol.verma4@gmail.com>
2022-11-16 09:42:54 +0530

    remove build

--- build/capillaryios.build/Release-iphoneos/capillaryios.build/Script-89B4387F2923B52500581DA2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copy Bridging Header to include directory.\n# This should be done automatically, but there is a bug in Xcode:\n# https://developer.apple.com/forums/thread/89209
-
-targetDir=${BUILT_PRODUCTS_DIR}/include/${PRODUCT_MODULE_NAME}/
-mkdir -p $targetDir
-cp ${DERIVED_SOURCES_DIR}/*-Swift.h ${targetDir}
-
-
